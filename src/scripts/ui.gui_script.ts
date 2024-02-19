5eb8fb937777c8bb99af628261d6905d752f48d7
thinknathan <github@thinknathan.com>
2024-02-18 22:12:07 -0800

    Update for new lint rules

--- src/scripts/ui.gui_script.ts
+++ src/scripts/ui.gui_script.ts
@@ -7,7 +7,7 @@ export function init(this: props): void {
 }
 
 export function on_message(this: props, messageId: hash, message: {score: number}, _sender: url): void {
-    if (messageId == hash("add_score")) {
+    if (messageId === hash("add_score")) {
         this.score += message.score;
         const scoreNode = gui.get_node("score");
         gui.set_text(scoreNode, `SCORE: ${this.score}`);
